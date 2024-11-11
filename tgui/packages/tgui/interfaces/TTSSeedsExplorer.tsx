9640ef87151031bc063c5c46887ff0976d3cf4e6
PhantornRU <stalkerlesha2@mail.ru>
2024-11-11 20:02:30 +1000

    TGUI Fix by Aylongo

--- tgui/packages/tgui/interfaces/TTSSeedsExplorer.tsx
+++ tgui/packages/tgui/interfaces/TTSSeedsExplorer.tsx
@@ -1,7 +1,20 @@
 import { BooleanLike } from 'common/react';
-import { useBackend, useLocalState } from '../backend';
-import { Button, LabeledList, Table, Section, Dropdown, Input, BlockQuote, Box, Icon, Stack } from '../components';
-import { VirtualList } from '../components/VirtualList';
+import { useState } from 'react';
+
+import { useBackend } from '../backend';
+import {
+  BlockQuote,
+  Box,
+  Button,
+  Dropdown,
+  Icon,
+  Input,
+  LabeledList,
+  Section,
+  Stack,
+  Table,
+  VirtualList,
+} from '../components';
 import { Window } from '../layouts';
 
 type Seed = {
@@ -41,36 +54,44 @@ const gender = {
 };
 
 const gendersIcons = {
-  'Мужской': {
+  Мужской: {
     icon: 'mars',
     color: 'blue',
   },
-  'Женский': {
+  Женский: {
     icon: 'venus',
     color: 'purple',
   },
-  'Любой': {
+  Любой: {
     icon: 'venus-mars',
     color: 'white',
   },
 };
 
-const getCheckboxGroup = (itemsList, selectedList, setSelected, contentKey = null) => {
+const getCheckboxGroup = (
+  itemsList,
+  selectedList,
+  setSelected,
+  contentKey = '',
+) => {
   return itemsList.map((item) => {
     const title = item[contentKey] ?? item;
     return (
       <Button.Checkbox
         key={title}
         checked={selectedList.includes(item)}
-        content={title}
         onClick={() => {
           if (selectedList.includes(item)) {
-            setSelected(selectedList.filter((i) => (i[contentKey] ?? i) !== item));
+            setSelected(
+              selectedList.filter((i) => (i[contentKey] ?? i) !== item),
+            );
           } else {
             setSelected([item, ...selectedList]);
           }
         }}
-      />
+      >
+        {title}
+      </Button.Checkbox>
     );
   });
 };
@@ -87,44 +108,71 @@ export const TTSSeedsExplorer = () => {
   );
 };
 
-export const TTSSeedsExplorerContent = (props, context) => {
-  const { act, data } = useBackend<TTSData>(context);
+export const TTSSeedsExplorerContent = () => {
+  const { act, data } = useBackend<TTSData>();
 
-  const { providers, seeds, selected_seed, phrases, donator_level, character_gender } = data;
+  const {
+    providers,
+    seeds,
+    selected_seed,
+    phrases,
+    donator_level,
+    character_gender,
+  } = data;
 
   const categories = seeds
     .map((seed) => seed.category)
     .filter((category, i, a) => a.indexOf(category) === i)
     .sort((a, b) => a.localeCompare(b));
-  const genders = seeds.map((seed) => seed.gender).filter((gender, i, a) => a.indexOf(gender) === i);
+  const genders = seeds
+    .map((seed) => seed.gender)
+    .filter((gender, i, a) => a.indexOf(gender) === i);
   const donatorLevels = seeds
     .map((seed) => seed.required_donator_level)
     .filter((level, i, a) => a.indexOf(level) === i)
     .sort((a, b) => a - b)
     .map((level) => donatorTiers[level]);
 
-  const [selectedProviders, setSelectedProviders] = useLocalState(context, 'selectedProviders', providers);
-  const [selectedGenders, setSelectedGenders] = useLocalState(
-    context,
-    'selectedGenders',
-    genders.includes(gender[character_gender]) ? [gender[character_gender]] : genders
+  const [selectedProviders, setSelectedProviders] = useState(providers);
+  const [selectedGenders, setSelectedGenders] = useState(
+    genders.includes(gender[character_gender])
+      ? [gender[character_gender]]
+      : genders,
   );
-  const [selectedCategories, setSelectedCategories] = useLocalState(context, 'selectedCategories', categories);
-  const [selectedDonatorLevels, setSelectedDonatorLevels] = useLocalState(
-    context,
-    'selectedDonatorLevels',
+  const [selectedCategories, setSelectedCategories] = useState(categories);
+  const [selectedDonatorLevels, setSelectedDonatorLevels] = useState(
     donatorLevels.includes(donatorTiers[donator_level])
-      ? donatorLevels.slice(0, donatorLevels.indexOf(donatorTiers[donator_level]) + 1)
-      : donatorLevels
+      ? donatorLevels.slice(
+          0,
+          donatorLevels.indexOf(donatorTiers[donator_level]) + 1,
+        )
+      : donatorLevels,
   );
-  const [selectedPhrase, setSelectedPhrase] = useLocalState(context, 'selectedPhrase', phrases[0]);
-  const [searchtext, setSearchtext] = useLocalState(context, 'searchtext', '');
-  const [searchToggle, setSearchToggle] = useLocalState(context, 'searchToggle', false);
+  const [selectedPhrase, setSelectedPhrase] = useState(phrases[0]);
+  const [searchtext, setSearchtext] = useState('');
+  const [searchToggle, setSearchToggle] = useState(false);
 
-  let providerCheckboxes = getCheckboxGroup(providers, selectedProviders, setSelectedProviders, 'name');
-  let genderesCheckboxes = getCheckboxGroup(genders, selectedGenders, setSelectedGenders);
-  let categoriesCheckboxes = getCheckboxGroup(categories, selectedCategories, setSelectedCategories);
-  let donatorLevelsCheckboxes = getCheckboxGroup(donatorLevels, selectedDonatorLevels, setSelectedDonatorLevels);
+  let providerCheckboxes = getCheckboxGroup(
+    providers,
+    selectedProviders,
+    setSelectedProviders,
+    'name',
+  );
+  let genderesCheckboxes = getCheckboxGroup(
+    genders,
+    selectedGenders,
+    setSelectedGenders,
+  );
+  let categoriesCheckboxes = getCheckboxGroup(
+    categories,
+    selectedCategories,
+    setSelectedCategories,
+  );
+  let donatorLevelsCheckboxes = getCheckboxGroup(
+    donatorLevels,
+    selectedDonatorLevels,
+    setSelectedDonatorLevels,
+  );
 
   let phrasesSelect = (
     <Dropdown
@@ -137,7 +185,13 @@ export const TTSSeedsExplorerContent = (props, context) => {
 
   let searchBar = (
     <>
-      {searchToggle && <Input placeholder="Название..." width={20} onInput={(e, value) => setSearchtext(value)} />}
+      {searchToggle && (
+        <Input
+          placeholder="Название..."
+          width={20}
+          onInput={(e, value) => setSearchtext(value)}
+        />
+      )}
       <Button
         icon="magnifying-glass"
         tooltip="Переключить поиск"
@@ -165,50 +219,77 @@ export const TTSSeedsExplorerContent = (props, context) => {
         selectedProviders.some((provider) => provider.name === seed.provider) &&
         selectedGenders.includes(seed.gender) &&
         selectedCategories.includes(seed.category) &&
-        selectedDonatorLevels.includes(donatorTiers[seed.required_donator_level]) &&
-        seed.name.toLowerCase().includes(searchtext.toLowerCase())
+        selectedDonatorLevels.includes(
+          donatorTiers[seed.required_donator_level],
+        ) &&
+        seed.name.toLowerCase().includes(searchtext.toLowerCase()),
     );
 
   let seedsRow = availableSeeds.map((seed) => {
     return (
-      <Table.Row key={seed.name} backgroundColor={selected_seed === seed.name ? 'green' : 'transparent'}>
+      <Table.Row
+        key={seed.name}
+        backgroundColor={selected_seed === seed.name ? 'green' : 'transparent'}
+      >
         <Table.Cell collapsing textAlign="center">
           <Button
             fluid
             color={selected_seed === seed.name ? 'green' : 'transparent'}
-            content={selected_seed === seed.name ? 'Выбрано' : 'Выбрать'}
-            tooltip={donator_level < seed.required_donator_level && 'Требуется более высокий уровень подписки'}
+            tooltip={
+              donator_level < seed.required_donator_level &&
+              'Требуется более высокий уровень подписки'
+            }
             onClick={() => act('select', { seed: seed.name })}
-          />
+          >
+            {selected_seed === seed.name ? 'Выбрано' : 'Выбрать'}
+          </Button>
         </Table.Cell>
         <Table.Cell collapsing textAlign="center">
           <Button
             fluid
             icon="music"
             color={selected_seed === seed.name ? 'green' : 'transparent'}
-            content=""
             tooltip="Прослушать пример"
-            onClick={() => act('listen', { seed: seed.name, phrase: selectedPhrase })}
+            onClick={() =>
+              act('listen', { seed: seed.name, phrase: selectedPhrase })
+            }
           />
         </Table.Cell>
         <Table.Cell
           bold
-          textColor={seed.required_donator_level > 0 && selected_seed !== seed.name ? 'orange' : 'white'}
+          textColor={
+            seed.required_donator_level > 0 && selected_seed !== seed.name
+              ? 'orange'
+              : 'white'
+          }
         >
           {seed.name}
         </Table.Cell>
-        <Table.Cell collapsing opacity={selected_seed === seed.name ? 0.5 : 0.25} textAlign="left">
+        <Table.Cell
+          collapsing
+          opacity={selected_seed === seed.name ? 0.5 : 0.25}
+          textAlign="left"
+        >
           {seed.category}
         </Table.Cell>
         <Table.Cell
           collapsing
           opacity={0.5}
-          textColor={selected_seed === seed.name ? 'white' : gendersIcons[seed.gender].color}
+          textColor={
+            selected_seed === seed.name
+              ? 'white'
+              : gendersIcons[seed.gender].color
+          }
           textAlign="left"
         >
           <Icon mx={1} size={1.2} name={gendersIcons[seed.gender].icon} />
         </Table.Cell>
-        <Table.Cell collapsing opacity={0.5} textColor="white" textAlign="right">
+        <Table.Cell
+          collapsing
+          opacity={0.5}
+          textColor="white"
+          textAlign="right"
+        >
           {seed.required_donator_level > 0 && (
             <>
               {donatorTiers[seed.required_donator_level]}
@@ -227,10 +308,18 @@ export const TTSSeedsExplorerContent = (props, context) => {
           <Stack.Item>
             <Section fill title="Фильтры">
               <LabeledList>
-                <LabeledList.Item label="Провайдеры">{providerCheckboxes}</LabeledList.Item>
-                <LabeledList.Item label="Пол">{genderesCheckboxes}</LabeledList.Item>
-                <LabeledList.Item label="Тир">{donatorLevelsCheckboxes}</LabeledList.Item>
-                <LabeledList.Item label="Фраза">{phrasesSelect}</LabeledList.Item>
+                <LabeledList.Item label="Провайдеры">
+                  {providerCheckboxes}
+                </LabeledList.Item>
+                <LabeledList.Item label="Пол">
+                  {genderesCheckboxes}
+                </LabeledList.Item>
+                <LabeledList.Item label="Тир">
+                  {donatorLevelsCheckboxes}
+                </LabeledList.Item>
+                <LabeledList.Item label="Фраза">
+                  {phrasesSelect}
+                </LabeledList.Item>
               </LabeledList>
             </Section>
           </Stack.Item>
@@ -276,7 +365,12 @@ export const TTSSeedsExplorerContent = (props, context) => {
         </Stack>
       </Stack.Item>
       <Stack.Item grow>
-        <Section fill scrollable title={`Голоса (${availableSeeds.length}/${seeds.length})`} buttons={searchBar}>
+        <Section
+          fill
+          scrollable
+          title={`Голоса (${availableSeeds.length}/${seeds.length})`}
+          buttons={searchBar}
+        >
           <Table>
             <VirtualList>{seedsRow}</VirtualList>
           </Table>
