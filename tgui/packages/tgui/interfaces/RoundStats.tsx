28494ea014bcb34df504c66c0463298082110635
Aylong <alexanderkitsa@gmail.com>
2025-02-20 14:38:48 +0200

    Stats restyle

--- tgui/packages/tgui/interfaces/RoundStats.tsx
+++ tgui/packages/tgui/interfaces/RoundStats.tsx
@@ -1,18 +1,15 @@
-import { useBackend } from '../backend';
-import { Collapsible, LabeledList, Section, Stack } from '../components';
-import { Window } from '../layouts';
+import { useState } from 'react';
 
-export const RoundStats = (props) => {
-  return (
-    <Window width={500} height={550}>
-      <Window.Content>
-        <Stack fill vertical>
-          <RoundStatsContent />
-        </Stack>
-      </Window.Content>
-    </Window>
-  );
-};
+import { useBackend } from '../backend';
+import {
+  Button,
+  LabeledList,
+  Modal,
+  Section,
+  Stack,
+  Tabs,
+} from '../components';
+import { Window } from '../layouts';
 
 type StatsData = {
   stats: Stats[];
@@ -48,120 +45,198 @@ type CasteStat = {
   stats: Stats;
 };
 
-export const RoundStatsContent = (props, context) => {
+export const RoundStats = (props) => {
   const { data } = useBackend<StatsData>();
   const { stats } = data;
+  const [tab, setTab] = useState(stats[0]);
+  const [modal, setModal] = useState();
+
+  const typeToIcon = (type: string) => {
+    switch (type) {
+      case 'Человек':
+        return 'person-rifle';
+      case 'Ксеноморф':
+        return 'spaghetti-monster-flying';
+    }
+  };
+
+  return (
+    <Window
+      width={400}
+      height={500}
+      theme={tab.title === 'Человек' ? 'crtgreen' : 'crtyellow'}
+    >
+      <Window.Content>
+        {!!modal && (
+          <KillFeedData
+            title={modal[0]}
+            killfeed={modal[1]}
+            setModal={setModal}
+          />
+        )}
+        <Stack fill vertical>
+          <Stack.Item>
+            <Tabs p={0} fluid textAlign="center">
+              {stats.map((stat) => (
+                <Tabs.Tab
+                  key={stat.title}
+                  icon={typeToIcon(stat.title)}
+                  selected={stat === tab}
+                  onClick={() => setTab(stat)}
+                >
+                  {stat.title}
+                </Tabs.Tab>
+              ))}
+            </Tabs>
+          </Stack.Item>
+          <Stack.Item grow overflowY="auto">
+            <RoundStatsContent
+              selectedTab={tab}
+              modal={modal}
+              setModal={setModal}
+            />
+          </Stack.Item>
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
+
+export const RoundStatsContent = (props) => {
+  const { data } = useBackend<StatsData>();
+  const { selectedTab, setModal } = props;
 
   return (
     <Stack fill vertical>
+      <Stack.Item>
+        <RegularStatData
+          total_kills={selectedTab.total_kills}
+          human_kills_total={selectedTab.human_kills_total}
+          xeno_kills_total={selectedTab.xeno_kills_total}
+          total_deaths={selectedTab.total_deaths}
+          steps_walked={selectedTab.steps_walked}
+          human_kill_feed={selectedTab.human_kill_feed}
+          xeno_kill_feed={selectedTab.xeno_kill_feed}
+          setModal={setModal}
+        />
+      </Stack.Item>
+      {!!selectedTab?.niche_stats && (
+        <Stack.Item>
+          <OtherStatData niche_stats={selectedTab.niche_stats} />
+        </Stack.Item>
+      )}
       <Stack.Item grow>
-        {stats.map((stat) => (
-          <Section key={stat.title}>
-            <Collapsible title={stat.title} open>
-              <Section>
-                <RegularStatData
-                  total_kills={stat.total_kills}
-                  human_kills_total={stat.human_kills_total}
-                  xeno_kills_total={stat.xeno_kills_total}
-                  total_deaths={stat.total_deaths}
-                  steps_walked={stat.steps_walked}
-                  niche_stats={stat.niche_stats}
-                  human_kill_feed={stat.human_kill_feed}
-                  xeno_kill_feed={stat.xeno_kill_feed}
-                />
-              </Section>
-              {stat.castes ? (
-                <XenoStatData
-                  total_hits={stat.total_hits}
-                  castes={stat.castes}
-                />
-              ) : (
-                <HumanStatData
-                  total_friendly_fire={stat.total_friendly_fire}
-                  total_revives={stat.total_revives}
-                  total_lives_saved={stat.total_lives_saved}
-                  total_shots={stat.total_shots}
-                  total_shots_hit={stat.total_shots_hit}
-                />
-              )}
-            </Collapsible>
-          </Section>
-        ))}
+        {selectedTab.castes ? (
+          <XenoStatData
+            total_hits={selectedTab.total_hits}
+            castes={selectedTab.castes}
+          />
+        ) : (
+          <HumanStatData
+            total_friendly_fire={selectedTab.total_friendly_fire}
+            total_revives={selectedTab.total_revives}
+            total_lives_saved={selectedTab.total_lives_saved}
+            total_shots={selectedTab.total_shots}
+            total_shots_hit={selectedTab.total_shots_hit}
+          />
+        )}
       </Stack.Item>
     </Stack>
   );
 };
 
-const RegularStatData = (props, context) => {
+const RegularStatData = (props) => {
   const { data } = useBackend<Stats>();
+
   const {
     total_kills,
     human_kills_total,
     xeno_kills_total,
     total_deaths,
     steps_walked,
-    niche_stats,
     human_kill_feed,
     xeno_kill_feed,
+    setModal,
   } = props;
 
   return (
-    <Collapsible title="Общая статистика" open>
-      <LabeledList>
-        {steps_walked && (
-          <LabeledList.Item label="Шагов сделано">
-            {steps_walked}
-          </LabeledList.Item>
+    <Section title="Общая статистика">
+      <Stack fill vertical mt={-1} mb={-1.33}>
+        <Stack.Item>
+          <LabeledList>
+            {steps_walked && (
+              <LabeledList.Item label="Шагов сделано">
+                {steps_walked}
+              </LabeledList.Item>
+            )}
+            {!!total_kills && (
+              <LabeledList.Item label="Всего убито">
+                {total_kills}
+              </LabeledList.Item>
+            )}
+
+            {!!total_deaths && (
+              <LabeledList.Item label="Количество смертей">
+                {total_deaths}
+              </LabeledList.Item>
+            )}
+          </LabeledList>
+        </Stack.Item>
+        {(!!human_kill_feed || !!xeno_kill_feed) && (
+          <Stack.Item>
+            <Stack fill textAlign="center">
+              {!!human_kill_feed && (
+                <Stack.Item grow>
+                  <Button
+                    fluid
+                    mb={1}
+                    onClick={() => setModal(['Убито людей', human_kill_feed])}
+                  >
+                    {`Убито людей: ${human_kills_total}`}
+                  </Button>
+                </Stack.Item>
+              )}
+              {!!xeno_kill_feed && (
+                <Stack.Item grow>
+                  <Button
+                    fluid
+                    mb={1}
+                    onClick={() =>
+                      setModal(['Убито ксеноморфов', xeno_kill_feed])
+                    }
+                  >
+                    {`Убито ксеноморфов: ${xeno_kills_total}`}
+                  </Button>
+                </Stack.Item>
+              )}
+            </Stack>
+          </Stack.Item>
         )}
-        {!!total_kills && (
-          <LabeledList.Item label="Всего убито">{total_kills}</LabeledList.Item>
-        )}
-        {!!human_kill_feed && (
-          <KillFeedData
-            title={'Убито людей: ' + human_kills_total}
-            killfeed={human_kill_feed}
-          />
-        )}
-        {!!xeno_kill_feed && (
-          <KillFeedData
-            title={'Убито ксеноморфов: ' + xeno_kills_total}
-            killfeed={xeno_kill_feed}
-          />
-        )}
-        {!!total_deaths && (
-          <LabeledList.Item label="Количество смертей">
-            {total_deaths}
-          </LabeledList.Item>
-        )}
-        {!!niche_stats &&
-          niche_stats.map((niche_stat) => (
+      </Stack>
+    </Section>
+  );
+};
+
+const OtherStatData = (props) => {
+  const { data } = useBackend<Stats>();
+  const { niche_stats } = props;
+
+  return (
+    <Section title="Прочая статистика">
+      <Stack fill vertical mt={-1} mb={-1.33}>
+        <LabeledList>
+          {niche_stats.map((niche_stat) => (
             <LabeledList.Item key={niche_stat.name} label={niche_stat.name}>
               {niche_stat.value}
             </LabeledList.Item>
           ))}
-      </LabeledList>
-    </Collapsible>
+        </LabeledList>
+      </Stack>
+    </Section>
   );
 };
 
-const KillFeedData = (props, context) => {
-  const { data } = useBackend<Statistic>();
-  const { title, killfeed } = props;
-
-  return (
-    <Collapsible title={title}>
-      <LabeledList>
-        {killfeed.map((kills) => (
-          <LabeledList.Item key={kills.name} label={kills.name}>
-            {kills.value}
-          </LabeledList.Item>
-        ))}
-      </LabeledList>
-    </Collapsible>
-  );
-};
-
-const HumanStatData = (props, context) => {
+const HumanStatData = (props) => {
   const { data } = useBackend<Stats>();
   const {
     total_friendly_fire,
@@ -172,69 +247,91 @@ const HumanStatData = (props, context) => {
   } = props;
 
   return (
-    <Collapsible title="Статистика человека" open>
-      <LabeledList>
-        {!!total_shots && (
-          <LabeledList.Item label="Выстрелов сделано">
-            {total_shots}
-          </LabeledList.Item>
-        )}
-        {!!total_shots && (
-          <LabeledList.Item label="Точность">
-            {Math.round((total_shots_hit / total_shots) * 100) + `%`}
-          </LabeledList.Item>
-        )}
-        {!!total_friendly_fire && (
-          <LabeledList.Item label="Попаданий по своим">
-            {total_friendly_fire}
-          </LabeledList.Item>
-        )}
-        {!!total_revives && (
-          <LabeledList.Item label="Количество воскрешений">
-            {total_revives}
-          </LabeledList.Item>
-        )}
-        {!!total_lives_saved && (
-          <LabeledList.Item label="Людей реанимировано">
-            {total_lives_saved}
-          </LabeledList.Item>
-        )}
-      </LabeledList>
-    </Collapsible>
+    <Section title="Статистика человека">
+      <Stack fill vertical mt={-1} mb={-1.33}>
+        <LabeledList>
+          {!!total_shots && (
+            <LabeledList.Item label="Выстрелов сделано">
+              {total_shots}
+            </LabeledList.Item>
+          )}
+          {!!total_shots && (
+            <LabeledList.Item label="Точность">
+              {Math.round((total_shots_hit / total_shots) * 100) + `%`}
+            </LabeledList.Item>
+          )}
+          {!!total_friendly_fire && (
+            <LabeledList.Item label="Попаданий по своим">
+              {total_friendly_fire}
+            </LabeledList.Item>
+          )}
+          {!!total_revives && (
+            <LabeledList.Item label="Количество воскрешений">
+              {total_revives}
+            </LabeledList.Item>
+          )}
+          {!!total_lives_saved && (
+            <LabeledList.Item label="Людей реанимировано">
+              {total_lives_saved}
+            </LabeledList.Item>
+          )}
+        </LabeledList>
+      </Stack>
+    </Section>
   );
 };
 
-const XenoStatData = (props, context) => {
+const XenoStatData = (props) => {
   const { data } = useBackend<Stats>();
   const { total_hits, castes } = props;
+  const [selectedCaste, setCaste] = useState(castes[0] || null);
 
   return (
-    <Collapsible title="Статистика ксеноморфа" open>
-      <LabeledList>
-        {!!total_hits && (
-          <LabeledList.Item label="Попаданий когтями">
-            {total_hits}
-          </LabeledList.Item>
-        )}
-      </LabeledList>
-      {!!castes &&
-        castes.map((caste) => (
+    <Stack fill vertical>
+      <Stack.Item>
+        <Section fitted title="Статистика ксеноморфа">
+          <Stack fill vertical>
+            {!!total_hits && (
+              <Stack.Item>
+                <LabeledList.Item label="Попаданий когтями">
+                  {total_hits}
+                </LabeledList.Item>
+              </Stack.Item>
+            )}
+            <Stack.Item>
+              <Tabs m={0} mb={-0.5}>
+                {castes?.map((caste) => (
+                  <Tabs.Tab
+                    key={caste.name}
+                    selected={selectedCaste?.name === caste.name}
+                    onClick={() => setCaste(caste)}
+                  >
+                    {caste.name}
+                  </Tabs.Tab>
+                ))}
+              </Tabs>
+            </Stack.Item>
+          </Stack>
+        </Section>
+      </Stack.Item>
+      <Stack.Item grow mt={0}>
+        <Section fill scrollable>
           <XenoCasteStats
-            key={caste.name}
-            caste_name={caste.name}
-            stats={caste.stats}
+            key={selectedCaste.name}
+            stats={selectedCaste.stats}
           />
-        ))}
-    </Collapsible>
+        </Section>
+      </Stack.Item>
+    </Stack>
   );
 };
 
-const XenoCasteStats = (props, context) => {
+const XenoCasteStats = (props) => {
   const { data } = useBackend<Stats>();
-  const { caste_name, stats } = props;
+  const { stats } = props;
 
   return (
-    <Collapsible title={caste_name}>
+    <Stack fill vertical m={1}>
       <RegularStatData
         total_kills={stats.total_kills}
         human_kills_total={stats.human_kills_total}
@@ -250,15 +347,53 @@ const XenoCasteStats = (props, context) => {
           {stats.total_hits}
         </LabeledList.Item>
       )}
+
       {!!stats.abilities_used && (
-        <Collapsible title="Способности">
-          {stats.abilities_used.map((ability) => (
-            <LabeledList.Item key={ability.name} label={ability.name}>
-              {ability.value}
-            </LabeledList.Item>
-          ))}
-        </Collapsible>
+        <Section title="Способности">
+          <Stack fill vertical mt={-1} mb={-1.33}>
+            <LabeledList>
+              {stats.abilities_used.map((ability) => (
+                <LabeledList.Item key={ability.name} label={ability.name}>
+                  {ability.value}
+                </LabeledList.Item>
+              ))}
+            </LabeledList>
+          </Stack>
+        </Section>
       )}
-    </Collapsible>
+    </Stack>
+  );
+};
+
+const KillFeedData = (props) => {
+  const { data } = useBackend<Statistic>();
+  const { title, killfeed, setModal } = props;
+
+  return (
+    <Modal p={1} width="80vw" height="50vh">
+      <Section
+        fill
+        scrollable
+        title={title}
+        buttons={
+          <Button
+            icon="times"
+            color="red"
+            m={-0.2}
+            onClick={() => setModal(null)}
+          />
+        }
+      >
+        <Stack fill vertical mt={-1} mb={-1.33}>
+          <LabeledList>
+            {killfeed.map((kills) => (
+              <LabeledList.Item key={kills.name} label={kills.name}>
+                {kills.value}
+              </LabeledList.Item>
+            ))}
+          </LabeledList>
+        </Stack>
+      </Section>
+    </Modal>
   );
 };
